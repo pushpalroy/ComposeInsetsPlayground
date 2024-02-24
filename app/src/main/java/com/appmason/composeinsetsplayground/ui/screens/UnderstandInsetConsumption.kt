29a5403798a93f4f1533bd217fa760be308349a7
Pushpal Roy <pushpalroy2007@gmail.com>
2024-02-24 03:02:05 -0600

    Add inset consumption example

--- app/src/main/java/com/appmason/composeinsetsplayground/ui/screens/UnderstandInsetConsumption.kt
+++ app/src/main/java/com/appmason/composeinsetsplayground/ui/screens/UnderstandInsetConsumption.kt
@@ -1,54 +1,45 @@
 package com.appmason.composeinsetsplayground.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsBottomHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.graphics.Color
 
 /**
  * https://developer.android.com/jetpack/compose/layouts/insets#inset-consumption
  */
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun UnderstandInsetConsumption() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .statusBarsPadding()
-    ) {
-        var textFieldValue by remember { mutableStateOf(TextFieldValue()) }
-        LazyColumn(
-            modifier = Modifier.imePadding(),
+    Scaffold { innerPadding ->
+        // innerPadding contains inset information for you to use and apply
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Blue)
+                .padding(innerPadding)
+                // Consume this insets so that it's not applied again when using safeDrawing in the hierarchy below
+                .consumeWindowInsets(innerPadding)
         ) {
-            items(50) { index ->
-                TextField(
-                    value = textFieldValue,
-                    onValueChange = { textFieldValue = it },
-                    modifier = Modifier.fillMaxWidth(),
-                    placeholder = {
-                        Text(text = "Item $index")
-                    }
-                )
-            }
-            item {
-                Spacer(
-                    Modifier.windowInsetsBottomHeight(
-                        WindowInsets.systemBars
-                    )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.Red)
+                    .windowInsetsPadding(WindowInsets.safeDrawing)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = Color.Green)
                 )
             }
         }
